e3167da94b65a76246c42685b8bec32caacfb9f7
Justin Stitt <jkstitt@csu.fullerton.edu>
2021-12-02 23:40:09 -0800

    🧊 updated djikstraShortestDistance
    removed visited vector and refactored logical expressions

--- cpp/search/djikstraShortestDistance.cc
+++ cpp/search/djikstraShortestDistance.cc
@@ -6,7 +6,6 @@ using namespace std;
 /* returns shortest path to e starting at s */
 int shortestPath(vector<vector<int>>& G, int s, int e) {
     vector<int> distances(G.size(), INT_MAX);
-    vector<bool> visited(G.size(), 0);
     distances[s] = 0; // distance to start from start is 0
     queue<int> Q;
     Q.push(s);
@@ -14,15 +13,13 @@ int shortestPath(vector<vector<int>>& G, int s, int e) {
     while(!Q.empty()) {
         int u = Q.front();
         Q.pop();
-        visited[u] = 1; // mark front of queue as visited
         // get all of adj of u
         for(int x{}; x < G[u].size(); ++x) {
-            if(G[u][x] && !visited[x]) {
+            if(!G[u][x]) continue;
+            if(distances[x] > distances[u] + G[u][x]) {
                 Q.push(x);
-                if(distances[x] > distances[u] + G[u][x]) {
-                    distances[x] = distances[u] + G[u][x];
-                }
-            }
+                distances[x] = distances[u] + G[u][x];
+            }                
         }
     
     }
@@ -34,14 +31,18 @@ int main() {
     vector<vector<int>> graph;
     graph.resize(5);
     for(auto& r : graph) r.resize(5);
-
+/*
     graph[0][1] = 2;
     graph[0][4] = 3;
     graph[1][2] = 1;
     graph[2][3] = 4;
     graph[3][1] = 6;
-
-    int sd = shortestPath(graph, 0, 1);
+*/
+    graph[0][1] = 8;
+    graph[0][2] = 1;
+    graph[2][1] = 1;
+    graph[1][3] = 3;
+    int sd = shortestPath(graph, 0, 3);
     std::cout << "shortest distance: " << sd << "\n";
 
     return 0;
